b0522f7f9a1ad0e743651c129bd0111a327b5303
CaueNorci <cnl5493@gmail.com>
2022-01-24 16:44:26 -0300

    esqueleto 2.0

---
+++ script.js
@@ -0,0 +1,9 @@
+let idade = document.getElementById('idade')
+let altura = document.getElementById('altura')
+let peso = document.getElementById('peso')
+let res = document.getElementById('res')
+let imc = 0
+
+function calcular(){
+    idade + altura + peso 
+}
