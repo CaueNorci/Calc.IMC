53955ae0e73e8c53993b752973bd2d5980066281
CaueNorci <cnl5493@gmail.com>
2022-01-25 13:56:00 -0300

    Atualização do arquivo JS

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-let idade = document.getElementById('idade')
-let altura = document.getElementById('altura')
-let peso = document.getElementById('peso')
-let res = document.getElementById('res')
-let imc = 0
+const calcular = document.getElementById('calcular');
 
-function calcular(){
-    idade + altura + peso 
+function imc(){
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const res = document.getElementById('res');
+
+    if(altura !== '' && peso !== ''){
+        const valorIMC = (peso / (altura * altura)).toFixed(1)
+
+        let classificacao = ''
+
+        if(valorIMC < 18,5){
+            classificacao = 'Abaixo do peso';
+
+        }else if (valorIMC < 25){
+            classificacao = 'Peso normal';
+
+        }else if (valorIMC < 30){
+            classificacao = 'Sobrepeso';
+
+        }else if (valorIMC < 35){
+            classificacao = 'Obesidade grau I';
+
+        }else if (valorIMC < 40){
+            classificacao = 'Obesidade grau II';
+
+        }else {
+            classificacao = 'Obesidade grau III (Mórbida)';
+        }
+
+        res.textContent = `Olá! Seu IMC é de ${valorIMC} e você está na seguinte classificação: ${classificacao}`
+
+    } else{
+        res.textContent = "Preencha todos os campos!"
+    }
+
 }
+
+calcular.addEventListener('click', imc);
